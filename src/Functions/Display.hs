92718f6beb589032d52d04b978743e303e5546d6
Jack Ellis <jackellis1504@gmail.com>
2022-10-24 09:40:59 +0100

    Update printing of TeamOrMultiples

--- src/Functions/Display.hs
+++ src/Functions/Display.hs
@@ -4,8 +4,13 @@ module Functions.Display where
 import Type
 import Data.List
 
+ppTeamOrMultiple :: TeamOrMultiple -> String
+ppTeamOrMultiple (Team t) = t
+ppTeamOrMultiple (MultipleTeam t i) = t ++ "x" ++ show i
+ppTeamOrMultiple (Teams ts) = intercalate "/" $ map show ts
+
 ppVariation :: Variation -> String
-ppVariation = intercalate "\n" . map (\(p, ts) -> p ++ ": " ++ show ts)
+ppVariation = intercalate "\n" . map (\(p, t) -> p ++ ": " ++ ppTeamOrMultiple t)
 
 ppVariations :: [Variation] -> String
 ppVariations = intercalate "\n---\n" . map ppVariation
