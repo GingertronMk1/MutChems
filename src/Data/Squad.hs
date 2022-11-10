12b537a55286455d50d474c9aab884de9fce60e9
Jack Ellis <jackellis1504@gmail.com>
2022-11-10 13:52:09 +0000

    Gonna get me a Zeitler

--- src/Data/Squad.hs
+++ src/Data/Squad.hs
@@ -80,5 +80,5 @@ strategy = ("None", [])
 prospectiveAdditions :: Lineup
 prospectiveAdditions =
   [
-  -- ("Devin Hester", [Team T.falcons, Team T.bears, Team T.seahawks, Team T.ravens, Team T.legends])
+    ("Kevin Zeitler", [Team T.bengals, Team T.browns, Team T.giants, Team T.ravens])
   ]
