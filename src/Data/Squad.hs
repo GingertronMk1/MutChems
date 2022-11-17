290bad1bd5f633ccd9cdc48cbe4e91adf7dd9bbf
Jack Ellis <jackellis1504@gmail.com>
2022-11-17 08:45:05 +0000

    I plan on picking up a couple of Harvest players

--- src/Data/Squad.hs
+++ src/Data/Squad.hs
@@ -84,6 +84,8 @@ strategy = ("None", [])
 prospectiveAdditions :: [ProspectiveAddition]
 prospectiveAdditions =
   [
+    Addition ("Khalil Mack", [Team T.raiders, Team T.bears, Team T.chargers]),
+    Addition ("Creed Humphrey", [Team T.chiefs])
     -- Addition ("Richard Seymour", [Team T.legends, Team T.patriots, Team T.raiders]),
     -- Addition ("Aaron Donald", [Team T.rams])
   ]
