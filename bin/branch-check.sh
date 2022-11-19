c1b66ccb00ab8a157ee0c8d4356b6a553b5cdd04
Jack Ellis <jackellis1504@gmail.com>
2022-11-19 11:49:37 +0000

    Amend file

--- bin/branch-check.sh
+++ bin/branch-check.sh
@@ -2,14 +2,16 @@
 
 git fetch origin
 
-if [ $(git branch | grep "$@") ]
+git branch
+
+if [ -z $(git branch | grep $@) ]
 then
-  echo "Branch $@ exists, doing nothing..."
-else
   CURRENT_BRANCH="$(git branch --show-current)"
   echo "No branch called $@, creating..."
   git checkout -b $@
   git push -u origin $@
   echo "Checking back out to $CURRENT_BRANCH"
   git checkout $CURRENT_BRANCH
+else
+  echo "Branch $@ exists, doing nothing..."
 fi
