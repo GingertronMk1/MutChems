ba56479c0c31e9383d09079d1c74a578762f8ff2
Jack Ellis <jackellis1504@gmail.com>
2022-11-20 13:40:38 +0000

    And that should be that

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,9 @@ import           Data.Calculated
 import           Functions.Display
 import           Functions.Domain
 
--- | Give me the best Variations given a Lineup (if there are no duplicate Players).
-main :: IO ()
-main = main'
-
 -- | Give me the best Variations given a Lineup.
-main' :: IO()
-main' = do
+main :: IO()
+main = do
   let fv = map (lineupToVariations . convertSquad) squads
-  let markdown = intercalation (genMarkdown . doubleFoldVariations) fv
-  writeFile "output.md" markdown
+  writeFile "output.md" . intercalation (genMarkdown . doubleFoldVariations) $ fv
   putStrLn . intercalation ppVariations $ fv
