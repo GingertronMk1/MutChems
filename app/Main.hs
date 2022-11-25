a28f863bb0f95c52da2b8c4ded54e995f10ef25d
Jack Ellis <jackellis1504@gmail.com>
2022-11-25 13:57:52 +0000

    Better printing of the number of options

--- app/Main.hs
+++ app/Main.hs
@@ -12,11 +12,12 @@ import           Functions.Domain
 main :: IO()
 main = do
   let iterated = iteratedProspectiveSquads
+  let prettyPrintedNumberOfOptions = map (ppNumber . numberOfOptionsFn . snd) $ iterated
   putStrLn $
     printf
       "Calculating best Variations out of %s options (each limited to a maximum of %s)"
       [
-        intercalate ", " . map (ppNumber . numberOfOptionsFn . snd) $ iterated,
+        intercalate ", " $ init prettyPrintedNumberOfOptions ++ ["and " ++ last prettyPrintedNumberOfOptions],
         ppNumber squadFilterThreshold
       ]
   writeFile "output.md"
