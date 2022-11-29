a8a1671608c3595317fa2e95e69210201a693f1c
Jack Ellis <jackellis1504@gmail.com>
2022-11-29 18:49:25 +0000

    Update format command

--- bin/lint-and-format.sh
+++ bin/lint-and-format.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-echo "Formatting files with stylish-haskell..."
-stylish-haskell -i -v -c .stylish-haskell.yaml ./**/*.hs
-echo "Formatting complete"
-echo "Linting files with hlint"
-hlint ./**/*.hs
-echo "Linting complete"
+stylish-haskell ./**/*.hs -i
