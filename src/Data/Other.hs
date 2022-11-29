7e80f607c8f2a02ee41f6199f3064a63cad825d8
Jack Ellis <jackellis1504@gmail.com>
2022-11-29 22:13:12 +0000

    Just updated my preference list for teams

--- src/Data/Other.hs
+++ src/Data/Other.hs
@@ -3,6 +3,12 @@
 -- Just useful bits and bobs - should maybe be an env file? Idk
 module Data.Other where
 
+import Data.Teams
+import Types.Basic
+
 -- | The maximum number of Variations per Lineup
 squadFilterThreshold :: Int
 squadFilterThreshold = 5000000
+
+preferences :: [Team]
+preferences = [legends, seahawks, eagles, raiders]
