1a76e7553d211df95eeb5b99d2fe8895f99c6d3d
Jack Ellis <jackellis1504@gmail.com>
2022-12-03 23:36:46 +0000

    Why not say how long it's been running?

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,21 @@ module Main (main) where
 import           Data.Calculated
 import           Data.List
 import           Data.Squad
+import           Functions.Application
 import           Functions.Display
 import           Types.ProspectiveChange
 import           Types.Variation
+import      Data.Time.Clock.System
 
 -- | Give me the best Variations given a Lineup.
 main :: IO()
 main = do
+  start <- getSystemTime
   writeFile "output.md"
     . intercalate "\n\n---\n\n"
     . printLineups
     . bestOfAllSquadsFn
     . addProspectivesInTurn prospectiveAdditions
     $ squadNoProspectives
-  putStrLn "Done!"
+  end <- getSystemTime
+  putStrLn $ printf "Done in %s seconds" [show (systemSeconds end - systemSeconds start)]
