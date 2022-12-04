cc0e3b571aff08c3e7f869c1361094e14ed0ea05
Jack Ellis <jackellis1504@gmail.com>
2022-12-04 12:27:51 +0000

    Chucked a couple more tests in

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,6 @@
 import           Test.HUnit
 
 import           Functions.Application
-import           Functions.Display
 
 main :: IO ()
 main = do
@@ -11,7 +10,9 @@ main = do
 testList :: Test
 testList = TestList [
     testRmDups,
-    testPPNumber
+    testPPNumber,
+    testUnbreakSpaces,
+    testDistanceFrom5
   ]
 
 testRmDups :: Test
@@ -41,3 +42,17 @@ testPPNumber' s =
   in case thisString of
     [_,_,_,c] -> assertEqual "C is a comma" c ',' : testPPNumber' theRest
     s'        -> [assertBool "Does not contain a comma" (',' `notElem` s')]
+
+testUnbreakSpaces :: Test
+testUnbreakSpaces = TestCase $ assertEqual
+  "replaceSpaces should replace all spaces with &nbsp;"
+  "Hello,&nbsp;World!"
+  (unBreakSpaces "Hello, World!")
+
+testDistanceFrom5 :: Test
+testDistanceFrom5 = TestCase $ do
+  assertEqual "distanceFrom5 should return 0 for multiples of 5" 0 (distanceFrom5 5)
+  assertEqual "distanceFrom5 should return 1 for numbers one less than a multiple of 5" 1 (distanceFrom5 4)
+  assertEqual "distanceFrom5 should return 2 for numbers two less than a multiple of 5" 2 (distanceFrom5 3)
+  assertEqual "distanceFrom5 should return 2 for numbers three less than a multiple of 5" 2 (distanceFrom5 2)
+  assertEqual "distanceFrom5 should return 1 for numbers four less than a multiple of 5" 1 (distanceFrom5 1)
