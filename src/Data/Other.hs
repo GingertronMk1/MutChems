f71c7847f866f905ad032359bf19ee0983790b0d
Jack Ellis <jackellis1504@gmail.com>
2022-12-04 11:43:32 +0000

    Formatting

--- src/Data/Other.hs
+++ src/Data/Other.hs
@@ -1,10 +1,10 @@
 -- | Module: Data.Other
--- 
+--
 -- Just useful bits and bobs - should maybe be an env file? Idk
 module Data.Other where
 
-import Data.Teams
-import Types.Basic
+import           Data.Teams
+import           Types.Basic
 
 -- | The maximum number of Variations per Lineup
 squadFilterThreshold :: Int
