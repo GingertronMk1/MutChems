4f0d3c3150e7079f19e84faf81132b584b8ed2a3
Jack Ellis <jackellis1504@gmail.com>
2022-12-11 13:09:39 +0000

    Update tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,29 +9,9 @@ main = do
 
 testList :: Test
 testList = TestList [
-    testPPNumber,
     testUnbreakSpaces,
     testDistanceFrom5
   ]
-
-testPPNumber :: Test
-testPPNumber =
-  let testDataList = map (10^) ([1..100] :: [Integer]) :: [Integer]
-   in TestList
-      $ map (
-        TestList
-        . map TestCase
-        . testPPNumber'
-        . reverse
-        . ppNumber
-        ) testDataList
-
-testPPNumber' :: String -> [Assertion]
-testPPNumber' s =
-  let (thisString, theRest) = splitAt 4 s
-  in case thisString of
-    [_,_,_,c] -> assertEqual "C is a comma" c ',' : testPPNumber' theRest
-    s'        -> [assertBool "Does not contain a comma" (',' `notElem` s')]
 
 testUnbreakSpaces :: Test
 testUnbreakSpaces = TestCase $ assertEqual
