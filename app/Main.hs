ff928879ea4f8937be3ea707f49360f07fbe8cda
Jack Ellis <jackellis1504@gmail.com>
2023-01-30 00:49:12 +0000

    Gimme feedback

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,9 @@ import Types.Player (genHTML)
 
 -- | Give me the best Variations given a Lineup.
 main :: IO ()
-main =
+main = do
   genHTML
     "input.json"
     "output.md"
     squadFilterThreshold
+  putStrLn "Done"
