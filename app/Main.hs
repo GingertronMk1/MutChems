fd32e601635876e8fdcaf7c18c8d4154733c7316
Jack Ellis <jackellis1504@gmail.com>
2023-02-01 18:07:59 +0000

    idk

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,6 @@ import Types.BuildObject
 import Types.DisplayObject
 import Types.InitObject
 import Types.Lineup
-import Types.Player
-import Types.Variation (flatLineupToVariations)
 
 -- | Give me the best Variations given a Lineup.
 main :: IO ()
@@ -51,13 +49,3 @@ genHTML
             . head
             $ buildObjects
     writeFile outputFile $ html ++ "\n" ++ markdownTables
-
-testing = do
-  (JSONInitObject {groupedLineup = gl, prospectiveChanges = pcs}) <- decodeJSONInitObject "input.json"
-  let lastLineup = last . iterativelyApplyProspectiveChanges pcs . flattenGroupedLineup $ gl
-  let filteredLineup = buildObjectLineup . filterOutTeams ["Legends"] $ lastLineup
-  let reducedLineup = reduceFlatLineupRecursive 10000 filteredLineup
-  print filteredLineup
-  print reducedLineup
-  print . product . map (length . playerTeams) $ reducedLineup
-  print . maximum . flatLineupToVariations $ reducedLineup
