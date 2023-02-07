4acf175413517afc0d75a4e109947e184abf5288
Jack Ellis <jackellis1504@gmail.com>
2023-02-07 19:23:16 +0000

    Why was this a function again?

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@
 -- Module: Main
 module Main (main) where
 
-import Classes.Data
 import Functions.Application
 import System.Environment
 import Text.Printf
@@ -16,33 +15,27 @@ import Types.Lineup
 main :: IO ()
 main = do
   args <- getArgs
-  argumentList <- compileArgumentListAndPrintResults args
-  let inputFileName = argInputFile argumentList
-  let stepCount = argStepCount argumentList
-  initObject <- openAndStepInitObject inputFileName stepCount
-  genHTML initObject argumentList
-  putStrLn "Done"
-
-genHTML :: InitObject -> ArgumentList -> IO ()
-genHTML
-  initObject
-  ( ArgumentList
-      { argFilterThreshold = filterThreshold,
-        argDisregardTeams = disregardTeams,
-        argOutputFile = outputFile
-      }
-    ) = do
-    let buildObjects = initObjectToBuildObjects initObject
-    let intermediateObjects = map (buildObjectToIntermediateObject filterThreshold . filterOutTeams disregardTeams) buildObjects
-    putStrLn $ printf "Filtered all lineups and converted to a total of %d Variations" (sum . map (length . iObjVariations) $ intermediateObjects)
-    let displayObjects = map intermediateObjectToDisplayObject intermediateObjects
-    let html =
-          wrapInTag "table"
-            . wrapInTag "tr"
-            $ printDisplayObjectsAsHtmlTable displayObjects
-    let markdownTables =
-          printPlayersBelongingToTeamsToMarkdown
-            . buildObjectLineup
-            . head
-            $ buildObjects
-    writeFile outputFile $ html ++ "\n" ++ markdownTables
+  ArgumentList
+    { argFilterThreshold = filterThreshold,
+      argDisregardTeams = disregardTeams,
+      argOutputFile = outputFile,
+      argInputFile = inputFile,
+      argStepCount = stepCount
+    } <-
+    compileArgumentListAndPrintResults args
+  initObject <- openAndStepInitObject inputFile stepCount
+  let buildObjects = initObjectToBuildObjects initObject
+  let intermediateObjects = map (buildObjectToIntermediateObject filterThreshold . filterOutTeams disregardTeams) buildObjects
+  putStrLn $ printf "Filtered all lineups and converted to a total of %d Variations" (sum . map (length . iObjVariations) $ intermediateObjects)
+  let displayObjects = map intermediateObjectToDisplayObject intermediateObjects
+  let html =
+        wrapInTag "table"
+          . wrapInTag "tr"
+          $ printDisplayObjectsAsHtmlTable displayObjects
+  let markdownTables =
+        printPlayersBelongingToTeamsToMarkdown
+          . buildObjectLineup
+          . head
+          $ buildObjects
+  writeFile outputFile $ html ++ "\n" ++ markdownTables
+  putStrLn "Done!"
