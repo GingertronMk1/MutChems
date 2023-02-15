f82da694db99181c127aca18b5525ae833d45927
Jack Ellis <jackellis1504@gmail.com>
2023-02-15 19:25:35 +0000

    Formatting

--- src/Types/Basic.hs
+++ src/Types/Basic.hs
@@ -3,12 +3,11 @@
 -- Alias types
 module Types.Basic where
 
-
 -- | A string representation of a TeamOrMultiple
 type EncodedTeamOrMultiple = String
 
+-- | A string representation of a Position
 type EncodedPosition = String
 
 -- | Player is shorthand for a String - it is just the name of a football player.
 type PlayerName = String
-
