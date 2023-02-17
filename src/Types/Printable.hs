293ec888e26cdf02ec41d7a6834ceb0b481dedf5
Jack Ellis <jackellis1504@gmail.com>
2023-02-17 17:36:02 +0000

    Formatting and removal of unused function

--- src/Types/Printable.hs
+++ src/Types/Printable.hs
@@ -9,7 +9,7 @@ class Printable a where
     printf' :: String -> String -> a
 
 instance Printable String where
-    printf' s x = reverse s ++ x
+    printf' acc s = reverse acc ++ s
 
 instance {-# OVERLAPPING #-} (Printable r) => Printable (String -> r) where
     printf' acc "" _ = printf' "" acc
@@ -20,6 +20,3 @@ instance {-# OVERLAPPING #-} (Show x, Printable r) => Printable (x -> r) where
     printf' acc ('%':'s':ss) x = printf' (reverse (show x) ++ acc) ss
     printf' acc (s:ss) x = printf' (s:acc) ss x
     printf' acc "" _ = printf' acc ""
-
-pfTest :: IO ()
-pfTest = putStrLn $ printf "Test %s" "testington"
