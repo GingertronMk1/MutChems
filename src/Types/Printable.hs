5e7923d3f6d5396eb96599fbf5daef715cc03e9c
Jack Ellis <jackellis1504@gmail.com>
2023-03-01 09:58:34 +0000

    Reordered base case

--- src/Types/Printable.hs
+++ src/Types/Printable.hs
@@ -17,6 +17,6 @@ instance {-# OVERLAPPING #-} (Printable r) => Printable (String -> r) where
   printf' acc (s : ss) x = printf' (s : acc) ss x
 
 instance {-# OVERLAPPING #-} (Show x, Printable r) => Printable (x -> r) where
+  printf' acc "" _ = printf' "" acc
   printf' acc ('%' : 's' : ss) x = printf' (reverse (show x) ++ acc) ss
   printf' acc (s : ss) x = printf' (s : acc) ss x
-  printf' acc "" _ = printf' acc ""
